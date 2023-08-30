af65e8978428b215ab0ee4eddd0beaf171dcf679
korivadaDivya <143597431+korivadaDivya@users.noreply.github.com>
2023-08-30 15:21:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import os
+import pandas as pd
+import joblib as jb
+
+heading_style = '''
+<div style="color:red;" align='center'>
+<h1>Sales Prediction</h1>
+</div>
+'''
+def return_df(tv,
+radio,
+newspaper):
+    kbn={
+    'tv':[tv],
+    'radio':[radio],
+    'newpaper':[newspaper],
+    }   
+    final_df=pd.DataFrame(kbn)
+    return final_df
+
+
+def base_model():
+    bmodel=jb.load(os.path.join('finalized_model.pkl'))
+    return bmodel
+
+st.markdown(heading_style, unsafe_allow_html=True)
+tv=st.number_input('how much money was on tv advertisement ', min_value=0)
+radio=st.number_input('how much money was on radio advertisement ', min_value=0)
+newspaper=st.number_input('how much money was on newspaper advertisement ', min_value=0)
+df=return_df(tv,
+radio,
+newspaper)
+if st.button('Submit'):
+	model=base_model()
+	preds=model.predict(df)
+	predictions=preds[0]
+		st.write('predictions')
